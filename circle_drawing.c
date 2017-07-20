7a337a2b7415b1e740ad6e3a452b6e6cb1a15b3c
deepanker-sachdeva <ds.deepankersachdeva@gmail.com>
2017-07-20 14:45:58 +0530

    Add files via upload

---
+++ circle_drawing.c
@@ -0,0 +1,86 @@
+/*Write a program to draw a point of width 10 pixel*/
+#include<GL/glut.h>
+#include<stdio.h>
+#include<math.h>
+void MyInit ( void ) {
+
+glClearColor ( 0.0, 0.0, 0.0, 0.0 ); //white background
+glColor3f(0.0f, 1.0f,0.0f); // green drawing colour
+glPointSize(5.0); // 5 pixel dot!
+glMatrixMode ( GL_PROJECTION );
+glLoadIdentity ( );
+gluOrtho2D ( 0.0, (GLdouble)500, 0.0, (GLdouble)500 ); //Display area
+}
+int main(int argc, char **argv)
+{
+
+glutInit(&argc,argv);
+glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
+
+int r,xc,yc,x,y;
+printf("Enter radius and centre of the circle!\n");
+scanf("%d%d%d",&r,&xc,&yc);
+
+glutInitWindowPosition(50,25);
+glutInitWindowSize(500,500);
+glutCreateWindow("Green window");
+MyInit ( );
+
+glClear ( GL_COLOR_BUFFER_BIT ); //clear pixel buffer
+glBegin(GL_POINTS); // render with points
+
+x=0;
+y=r;
+int p=1-r,x2,y2,x3,y3,x4,y4,x5,y5,x6,y6,x7,y7,x8,y8;
+while(x<y)
+{
+
+
+x2=y+xc;
+y2=x+yc;
+x3=y+xc;
+y3=-x+yc;
+x4=x+xc;
+y4=-y+yc;
+x5=-x+xc;
+y5=-y+yc;
+x6=-y+xc;
+y6=-x+yc;
+x7=-y+xc;
+y7=x+yc;
+x8=-x+xc;
+y8=y+yc;
+
+glVertex2i(x+xc,y+yc);
+glVertex2i(x2,y2);
+glVertex2i(x3,y3);
+glVertex2i(x4,y4);
+glVertex2i(x5,y5);
+glVertex2i(x6,y6);
+glVertex2i(x7,y7);
+glVertex2i(x8,y8);
+
+
+
+if(p<0)
+{
+x=x+1;
+p=p+2*x+1;
+}
+else
+{
+x=x+1;
+y=y-1;
+p=p+2*x+1-2*y;
+}
+
+}
+
+
+
+glEnd();
+glFlush();
+
+glutMainLoop();
+return 0;
+}
